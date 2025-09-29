2160017212807b7ce3502e09fc83dc7b0ac46c9d
xavierfingers <xavierfingers@gmail.com>
2025-09-28 19:34:07 -0600

    Commit
    moved file contents from content/posts/blog.md to index.md
    
    Removed index.html
    
    Create static.yml
    
    Create jekyll-gh-pages.yml Co-Authored-By: zainolabisi764-cell <zainolabisi764@gmail.com>
    
    7
    
    Created files for gemini
    Created CMakeLists.txt
    Forked Linux
    
    Create C# files for game
    
    .

---
+++ http-client.sh
@@ -0,0 +1,17 @@
+exec 3<>/dev/tcp/ysap.daveeddy.com/80
+
+lines=(
+   'GET /ping HTTP/1.1'
+   'Host: ysap.daveeddy.com'
+   'Connection: close'
+   ''
+   
+   )
+printf '%s\r\n' "${lines[@]}" >&3
+
+while read -r data <&3; do
+   [[ -z "$data" ]] && break
+done
+
+exec 3>&-
+
