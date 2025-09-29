423fdd71027c6ab8d87a689711735bf586fd48f4
xavierfingers <xavierfingers@gmail.com>
2025-09-28 19:34:07 -0600

    Commit Commit Commit Commit moved file contents from content/posts/blog.md to index.md
    Removed index.html
    
    Create static.yml
    
    Create jekyll-gh-pages.yml Co-Authored-By: zainolabisi764-cell <zainolabisi764@gmail.com>
    
    7
    
    Created files for gemini
    Created CMakeLists.txt
    Forked Linux
    
    Create C# files for game
    
    .

--- wocky.py
+++ wocky.py
@@ -1,5 +1,19 @@
+<<<<<<< HEAD
 import math
 
 print(math.pi)
 
 
+=======
+import requests
+
+request = requests.get("http://pie.dev/pu")
+response = request.text
+
+if request.status_code == 200:
+    print(f"Success! here is data: \n{response}")
+if request.status_code == 404:
+    print("Page not found")
+if request.status_code == 501:
+   print("Not implemented")
+>>>>>>> 2160017 (Commit)
